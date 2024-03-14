a68c82d990e36377cf163a1e7b6833915b43fb32
nnminh-haise <nnminh.sam.1803@gmail.com>
2024-03-14 08:58:19 +0700

    demo dummy chat

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppGateway } from './app/app.gateway';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './users/users.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost/itmc-web-chat'),
-    AuthModule,
-    UserModule,
-  ],
+  imports: [MongooseModule.forRoot('mongodb://localhost/itmc-web-chat')],
   providers: [AppGateway],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const configService = app.get(ConfigService);
   app.useStaticAssets(join(__dirname, '..', 'static'));
   await app.listen(3000);
 }
