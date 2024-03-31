8b1df363bf8a74889ac0200ce9f930e57f4487bb
nnminh-haise <nnminh.sam.1803@gmail.com>
2024-03-31 15:19:15 +0700

    fix: add users.model.ts file

---
+++ src/users/users.model.ts
@@ -0,0 +1,38 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+// *  The `@Schema` marked the class as a schema for the expecting table in database
+@Schema()
+export class User {
+  // * The `@Prop` marked the variable as a properties of a table
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  first_name: string;
+
+  @Prop({ required: true })
+  last_name: string;
+
+  @Prop({ required: true })
+  gender: string;
+
+  @Prop({ required: true })
+  dob: Date;
+
+  @Prop()
+  create_at: Date;
+
+  @Prop()
+  delete_at: Date;
+
+  @Prop()
+  refresh_token: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
